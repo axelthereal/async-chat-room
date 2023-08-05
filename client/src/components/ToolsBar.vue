0af348d6a5d56ddc7729e596378f9f7c7c1393ca
Axel Tokam <mail.axeltokam@gmail.com>
2023-08-05 02:10:51 +0100

    Implemented dynamic routing in toolbar

--- client/src/components/ToolsBar.vue
+++ client/src/components/ToolsBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="toolbar toggle">
+    <div class="toolbar">
      <!-- Toolbar-Header -->
        <div class="toolbar-header py-2" align="center">
        <img src="../assets/images/logo.png" class="cursor-p" alt="toolbar-logo">
@@ -7,25 +7,25 @@
 
      <!-- Toolbar-Menu -->  
      <div class="toolbar-menu list-group w-100 mt-4">
-       <div class="list-group-item rounded-0 cursor-p txt-dark-light d-flex justify-content-center align-items-center flex-column border-0 my-1">
+       <div @click="setActiveView('home')" :class="{ 'isactive':itemIssActive('home') }" class="list-group-item rounded-0 cursor-p txt-dark-light d-flex justify-content-center align-items-center flex-column border-0 my-1">
         <span class="item-icon title-b bi bi-house fs-5"></span>
         <span class="item-label text fw-medium fs-8  mt-1">Home</span>
        </div>
-       <div class="list-group-item rounded-0 cursor-p txt-dark-light d-flex justify-content-center align-items-center flex-column border-0 my-1">
+       <div @click="setActiveView('search')" :class="{ 'isactive':itemIssActive('search') }"  class="list-group-item rounded-0 cursor-p txt-dark-light d-flex justify-content-center align-items-center flex-column border-0 my-1">
         <span class="item-icon title-b bi bi-search fs-5"></span>
         <span class="item-label text fw-medium fs-8  mt-1">Search</span>
        </div>
-       <div class="list-group-item rounded-0 cursor-p txt-dark-light d-flex justify-content-center align-items-center flex-column border-0 my-1">
+       <div @click="setActiveView('groups')" :class="{ 'isactive':itemIssActive('groups') }"  class="list-group-item rounded-0 cursor-p txt-dark-light d-flex justify-content-center align-items-center flex-column border-0 my-1">
         <span class="item-icon title-b bi bi-chat-right-dots fs-5"></span>
         <span class="item-label text fw-medium fs-8  mt-1">Groups</span>
        </div>
-       <div class="list-group-item rounded-0 cursor-p txt-dark-light d-flex justify-content-center align-items-center flex-column border-0 my-1">
+       <div @click="setActiveView('saved')" :class="{ 'isactive':itemIssActive('saved') }"  class="list-group-item rounded-0 cursor-p txt-dark-light d-flex justify-content-center align-items-center flex-column border-0 my-1">
         <span class="item-icon title-b bi bi-bookmark fs-5"></span>
         <span class="item-label text fw-medium fs-8  mt-1">Saved</span>
        </div> 
-       <div class="list-group-item rounded-0 cursor-p txt-dark-light d-flex justify-content-center align-items-center flex-column border-0 my-1">
+       <div @click="setActiveView('archived')" :class="{ 'isactive':itemIssActive('archived') }"  class="list-group-item rounded-0 cursor-p txt-dark-light d-flex justify-content-center align-items-center flex-column border-0 my-1">
         <span class="item-icon title-b bi bi-archive fs-5"></span>
-        <span class="item-label text fw-medium fs-8  mt-1">Achived</span>
+        <span class="item-label text fw-medium fs-8  mt-1">Archived</span>
        </div>
      </div>
 
@@ -41,23 +41,37 @@
      </div>
 
     </div>
+
+    <!-- Toolbar-view -->
+    <div class="toolbar-view" v-if="activeView">
+       
+    </div>
 </template>
 
 <script> 
-
 export default {
     name: "ToolBar",
     data(){
       return {
-          appTheme: "dark" 
+          appTheme: "light",
+          activeView: null, 
       }
     },
+    components:{
+
+    },
      methods:{ 
-       getAppTheme(theme){
+        getAppTheme(theme){
           return this.appTheme == theme ? true : false;
         },
         toggleAppTheme(){
              this.appTheme = this.appTheme == "light" ? "dark" : "light";
+        },
+        itemIssActive(index){
+            return this.activeView == index ? true : false
+        },
+        setActiveView(target){
+            this.activeView = target;
         } 
      }   
 }
@@ -67,7 +81,7 @@ export default {
 <style scoped>
   .toolbar{ 
     position: relative;
-    min-width: 78px; 
+    min-width: 78px;  
     width:5%;  
     height: 100%;  
     background-color: var(--bg-light);
@@ -82,6 +96,10 @@ export default {
  }
  .toolbar .list-group-item-toggle{
     max-width: 50px;
+ }
+ .toolbar .isactive {
+    color: var(--tc-active);
+    transition:0.3s ease-out;
  }
  .toolbar .list-group-item-toggle .item-icon-btn{
    width: 35px;
@@ -107,5 +125,13 @@ export default {
     height:auto;
     border-radius: 50% !important;
     border: 2px solid var(--bd-light-dark);
+ }
+
+ .toolbar-view{
+    position: relative;
+    min-width: 78px; 
+    width:25%;  
+    height: 100%;  
+    background-color: var(--bg-light); 
  }
 </style>
