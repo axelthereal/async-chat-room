6c23f0d5f2444b94778679bfd24a8480933c5329
Axel Tokam <mail.axeltokam@gmail.com>
2023-08-06 14:17:29 +0100

    Added chatbox header

--- client/src/components/ChatBox.vue
+++ client/src/components/ChatBox.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="chatbox card border-0" id="chatBoxVue">
-        <div class="card-header border-0 bg-white"><h1>Chat header</h1></div>
+        <div class="card-header border-0 bg-white">
+           <div class="cb-info d-flex justify-content-between align-items-center gap-1">
+              <img src="../assets/images/user3.png" alt="" class="rounded-circle cursor-p" width="43" height="43" />
+              <div class="cb-data  w-100 bg-primary">info</div>
+              <div class="icons nowrap  bg-danger">icons</div>
+           </div>
+        </div>
         <div class="card-body border-0"> Messages here...</div>
         <div class="card-footer border-0"><h1>Chat Footer</h1></div>
     </div>
