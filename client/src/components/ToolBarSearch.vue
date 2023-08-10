b82713b73fe55bf5970e0e88ec93af60b7e0b4a2
Axel Tokam <mail.axeltokam@gmail.com>
2023-08-10 03:59:59 +0100

    Edited search view

--- client/src/components/ToolBarSearch.vue
+++ client/src/components/ToolBarSearch.vue
@@ -1,6 +1,9 @@
 <template>
-    <div> 
-         <h1>Search</h1>
+    <div class="viewcontent">
+     <div class="viewheader py-1 px-4">
+         <h1 class="fs-5 fw-semibold txt-black title-smb">Search</h1>
+         <span class="fs-7 txt-dark-lighter fw-medium text">Type your text to search for a contact or a group to join.</span>
+    </div>
     </div>
 </template>
 
@@ -11,4 +14,16 @@ export default {
 </script>
 
 <style scoped>
+ .viewcontent{
+     position:static;
+     overflow-y: scroll;
+     width: 100%;
+     height: 100vh;
+  }
+  .viewcontent::-webkit-scrollbar {
+    display: none !important;
+  }
+  .viewcontent{
+  -ms-overflow-style: none;
+  }
 </style>
