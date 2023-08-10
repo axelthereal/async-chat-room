914877667a26ff7aea0e4f1b5fab8cb7ef20def6
Axel Tokam <mail.axeltokam@gmail.com>
2023-08-10 04:01:01 +0100

    Edited groups view

--- client/src/components/ToolBarGroups.vue
+++ client/src/components/ToolBarGroups.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <h1>Groups</h1>
+    <div class="viewcontent">
+        <div class="viewheader py-1 px-4">
+         <h1 class="fs-5 fw-semibold txt-black title-smb">Groups</h1>
+        </div>
     </div>
 </template>
 
@@ -11,4 +13,16 @@ export default {
 </script>
 
 <style scoped>
+ .viewcontent{
+     position:static;
+     overflow-y: scroll;
+     width: 100%;
+     height: 100vh;
+  }
+  .viewcontent::-webkit-scrollbar {
+    display: none !important;
+  }
+  .viewcontent{
+  -ms-overflow-style: none;
+  }
 </style>
