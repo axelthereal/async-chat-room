38161096a038745654027710cc7c2e582758f25e
Axel Tokam <mail.axeltokam@gmail.com>
2023-08-10 04:03:58 +0100

    Added contacts, search, groups views

--- client/src/components/ToolBarContacts.vue
+++ client/src/components/ToolBarContacts.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <h1>Contacts</h1>
+    <div class="viewcontent">
+        <div class="viewheader py-1 px-4">
+         <h1 class="fs-5 fw-semibold txt-black title-smb">Contacts</h1>
+         <span class="fs-7 txt-dark-lighter fw-medium text">View your saved contacts</span>
+    </div>
     </div>
 </template>
 
@@ -11,4 +14,16 @@ export default {
 </script>
 
 <style scoped>
+.viewcontent{
+     position:static;
+     overflow-y: scroll;
+     width: 100%;
+     height: 100vh;
+  }
+  .viewcontent::-webkit-scrollbar {
+    display: none !important;
+  }
+  .viewcontent{
+  -ms-overflow-style: none;
+  }
 </style>
