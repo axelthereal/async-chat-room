539fcd2b4223d50e9af3c4cac1ea0138abaf0b5f
Axel Tokam <mail.axeltokam@gmail.com>
2023-08-10 02:26:38 +0100

    Adds media files link

--- client/src/components/ChatInfoBox.vue
+++ client/src/components/ChatInfoBox.vue
@@ -39,9 +39,79 @@
                 <span class="text fs-7 fw-medium">Media</span> 
                 <i class="bi bi-collection-play fs-5 txt-dark-lighter"></i> 
               </div>
-              <p class="desc-txt fs-8 text txt-dark-lighter fw-medium">
-              Media, links, and docs shared in the group
-              </p>
+             <!-- Nav pills -->
+<ul class="nav nav-tabs mx-0 border-0 gap-0 rounded-pill p-1 d-flex justify-content-between align-items-center" id="nav-tab" role="tablist">
+  <li class="nav-item d-flex align-items-center">
+    <a class="nav-link active rounded rounded-pill fs-8 text txt-dark-light fw-semibold" data-bs-toggle="tab" data-bs-target="#mediaProfile" role="tab" aria-controls="mediaProfile" aria-selected="true" href="#mediaProfile">Media</a>
+  </li>
+  <li class="nav-item d-flex align-items-center">
+    <a class="nav-link rounded rounded-pill fs-8 text txt-dark-light fw-semibold" data-bs-toggle="tab" data-bs-target="#filesProfile" role="tab" aria-controls="filesProfile" aria-selected="true" href="#filesProfile">Files</a>
+  </li>
+  <li class="nav-item d-flex align-items-center">
+    <a class="nav-link rounded rounded-pill fs-8 text txt-dark-light fw-semibold"  data-bs-toggle="tab" data-bs-target="#linksProfile" role="tab" aria-controls="linksProfile" aria-selected="true" href="#linksProfile">Links</a>
+  </li>
+</ul>
+
+<!-- Tab panes -->
+<div class="tab-content border-0" id="nav-tabContent">
+  <div class="tab-pane d-flex justify-content-between py-2 container fade active show flex-wrap" id="mediaProfile" role="tabpanel" aria-labelledby="mediaProfile-tab">
+   
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+   <span class="mediafile rounded-3 cursor-p m-1 ms-0">
+   <img src="../assets/cdn/ironman.jpg" alt="" srcset="">
+   </span>
+
+  </div>
+  <div class="tab-pane py-2 container fade active" id="filesProfile" role="tabpanel" aria-labelledby="filesProfile-tab">
+  Nothing to show...
+  </div>
+  <div class="tab-pane py-2 container fade active" id="linksProfile" role="tabpanel" aria-labelledby="linksProfile-tab">
+    Nothing to show...
+  </div>
+</div>
+
           </div>  
 
         </div>
@@ -64,6 +134,7 @@
     background-color: transparent; 
     border-left: 1px solid var(--bg-light-darker) !important;
   } 
+  
   .infobox-profile img{
     width: 93px;
     height: 93px;
@@ -73,5 +144,34 @@
   }
   #chatboxinfo .infobox-item{ 
    border-bottom:1px solid var(--bd-light) !important;
+  }
+  #chatboxinfo .groupmediaview .nav-tabs {
+       background-color: var(--bg-light-darker);
+  }
+  #chatboxinfo .groupmediaview .nav-tabs .nav-item{
+    width: 30%;
+  }
+  #chatboxinfo .groupmediaview .nav-tabs .nav-item a{
+    border: 1px solid var(--bd-light) !important;
+  }
+  #chatboxinfo .groupmediaview .nav-tabs .nav-item a.active{
+    color: var(--tc-active) !important;
+    background-color: var(--bg-light) !important;
+  }
+  #chatboxinfo .tab-content .mediafile{
+     position: relative;
+     width:30%;
+     height:50px;
+     background-color: green;
+     display: block; 
+     overflow: hidden;
+  }
+  #chatboxinfo .tab-content .mediafile img{
+     position: absolute;
+     top: 0;
+     left: 0;
+     object-fit: cover;
+     width:100%;
+     height:100%;
   }
 </style>
