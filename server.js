574eba4425b16cdd6589bbdd5f51acf9b91c9851
axelthereal <mail.axeltokam@gmail.com>
2024-06-28 19:09:48 +0100

    edited backend to route main app

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express, {static as static_} from "express";
+import express, { static as static_ } from "express";
 const app = express();
 import bodyparser from "body-parser";
 import session from "express-session";
@@ -9,31 +9,33 @@ import { fileURLToPath } from "url";
 // Global variables
 const port = 3030;
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); 
+const __dirname = path.dirname(__filename);
 
 // Template Engine
 app.set("view engine", "ejs");
 
 // Middlewares
-app.use(cors()); 
+app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
-app.use(session({
-       secret: "$node235Tvf23opml@sdf",
-       resave: false,
-       saveUninitialized: false, 
-       cookie: {secure:false}
-}));
+app.use(
+  session({
+    secret: "$node235Tvf23opml@sdf",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
 
-// Routing Handlers 
+// Routing Handlers
 //publicPath: process.env.NODE_ENV === 'production' ? '/' : ''
-app.use("/", express.static(path.join(__dirname, "client/dist")));
-app.use("/home", express.static(path.join(__dirname, "client/dist")));
-app.use("/signup", express.static(path.join(__dirname, "client/dist")));
-app.use("/signin", express.static(path.join(__dirname, "client/dist")));
-app.use("/chat", express.static(path.join(__dirname, "client/dist")));
+app.use("/", express.static(path.join(__dirname, "app/dist")));
+app.use("/home", express.static(path.join(__dirname, "app/dist")));
+app.use("/signup", express.static(path.join(__dirname, "app/dist")));
+app.use("/signin", express.static(path.join(__dirname, "app/dist")));
+app.use("/chat", express.static(path.join(__dirname, "app/dist")));
 
 // Listen to port
-app.listen(port, ()=>{
-    console.log(`Server running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
 });
